e648e6b1035827cfb13bc5887e11b49f07145e18
securiityy <daniel.dev.fi874007@gmail.com>
2024-07-30 09:18:53 -0300

    Update ip.py

--- flaskr/ip.py
+++ flaskr/ip.py
@@ -1,5 +1,5 @@
 def myip() -> str:
     iplocalhost = 'localhost'
-    ipuserphone = '192.168.100.30'
+    ipuserphone = '192.168.1.2'
     ipuserphonecompany = '192.168.1.11'
     return ipuserphone
